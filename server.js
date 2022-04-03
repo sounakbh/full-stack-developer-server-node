3dd1690dcc05e06b6ec1532b763c31d0fe72a6c1
sounakbh <sounakbh@gmail.com>
2022-04-03 13:47:29 -0400

    Added new packages and tuits file

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ import express from "express";
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/users-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
+import cors from "cors";
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 helloController(app);
 userController(app);
 tuitsController(app);
+
 app.get("/", (req, res) => {
   res.send("Welcome to Full Stack Development!");
 });
